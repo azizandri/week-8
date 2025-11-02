1f224cbdb1d620a7d3fd640b93a1a1a65868cdc7
azizandri <azizandri72@gmail.com>
2025-11-02 22:19:53 +0700

    class CreditCardPayment and BankTransferPayment

---
+++ src/week01/andri/id/ac/umn/BankTransferPayment.java
@@ -0,0 +1,17 @@
+package week01.andri.id.ac.umn;
+
+public class BankTransferPayment extends Payment {
+	
+	private String bankAccount;
+
+	public BankTransferPayment(double amount, String bankAccount) {
+		super(amount);
+		this.bankAccount = bankAccount;
+	}
+	
+	@Override
+	void processPayment() {
+		System.out.println("Processing bank transfer payment of $" + amount + " for bank account " + bankAccount);
+	}
+	
+}
