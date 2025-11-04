010f01cf0060b5ccd892d9f46f6cef21cdfe0123
azizandri <azizandri72@gmail.com>
2025-11-04 22:20:57 +0700

    Cash class

---
+++ src/week01/andri/id/ac/umn/Cash.java
@@ -0,0 +1,19 @@
+package week01.andri.id.ac.umn;
+
+public class Cash extends Payment {
+	public Cash(Item item) {
+		super(item);
+	}
+	
+	public int pay() {
+		if(isPaidOff) {
+			return 0;
+		}
+		isPaidOff = true;
+		return this.item.getPrice();
+	}
+	
+	public String getClassName() {
+		return "CASH";
+	}
+}
